c5be36cf4e720db598ee4fcc52c17d9639e63fce
OphaTapioka <ophashias@gmail.com>
2024-09-05 08:39:49 +0800

    build 1.4.3 round 2 upload

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Ophatapioka-Guess the Password-1.4.3";
+const contentToCache = [
+    "Build/guess 1.4.3.loader.js",
+    "Build/guess 1.4.3.framework.js",
+    "Build/guess 1.4.3.data",
+    "Build/guess 1.4.3.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
